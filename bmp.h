d54ed6bc2313cbcab560280971c46e7e87f47ad1
aneesh.damle <aneesh.damle@gmail.com>
2022-05-31 23:41:30 +0530

    Revised BMP headers

--- bmp.h
+++ bmp.h
@@ -1,48 +1,51 @@
-// BMP-related data types based on Microsoft's own
+#ifndef BMP_H
+#define BMP_H (1)
 
 #include <stdint.h>
 
-/**
- * Common Data Types
+/*! \brief This file includes BMP related datatypes
+ * \Note Microsoft Reference
+ */
+
+/*! \brief Common Data Types
  *
  * The data types in this section are essentially aliases for C/C++
  * primitive data types.
  *
- * Adapted from http://msdn.microsoft.com/en-us/library/cc230309.aspx.
- * See http://en.wikipedia.org/wiki/Stdint.h for more on stdint.h.
+ * \Ref http://msdn.microsoft.com/en-us/library/cc230309.aspx.
+ *      http://en.wikipedia.org/wiki/Stdint.h
  */
 typedef uint8_t  BYTE;
 typedef uint32_t DWORD;
 typedef int32_t  LONG;
 typedef uint16_t WORD;
 
-/**
- * BITMAPFILEHEADER
+
+/*! \brief BITMAPFILEHEADER
  *
  * The BITMAPFILEHEADER structure contains information about the type, size,
  * and layout of a file that contains a DIB [device-independent bitmap].
  *
- * Adapted from http://msdn.microsoft.com/en-us/library/dd183374(VS.85).aspx.
+ * \Ref http://msdn.microsoft.com/en-us/library/dd183374(VS.85).aspx.
  */
-typedef struct
+typedef struct BITMAPFILEHEADER
 {
     WORD   bfType;
     DWORD  bfSize;
     WORD   bfReserved1;
     WORD   bfReserved2;
     DWORD  bfOffBits;
-} __attribute__((__packed__))
-BITMAPFILEHEADER;
+} __attribute__((__packed__)) BITMAPFILEHEADER;
 
-/**
- * BITMAPINFOHEADER
+
+/*! \brief BITMAPINFOHEADER
  *
  * The BITMAPINFOHEADER structure contains information about the
  * dimensions and color format of a DIB [device-independent bitmap].
  *
- * Adapted from http://msdn.microsoft.com/en-us/library/dd183376(VS.85).aspx.
+ * \ref http://msdn.microsoft.com/en-us/library/dd183376(VS.85).aspx.
  */
-typedef struct
+typedef struct BITMAPINFOHEADER
 {
     DWORD  biSize;
     LONG   biWidth;
@@ -55,22 +58,24 @@ typedef struct
     LONG   biYPelsPerMeter;
     DWORD  biClrUsed;
     DWORD  biClrImportant;
-} __attribute__((__packed__))
-BITMAPINFOHEADER;
+} __attribute__((__packed__)) BITMAPINFOHEADER;
 
-/**
- * RGBTRIPLE
+
+
+/*! \brief RGBTRIPLE
  *
- * This structure describes a color consisting of relative intensities of
+ * \Note This structure describes a color consisting of relative intensities of
  * red, green, and blue.
+ * Alias to pixel
  *
- * Adapted from http://msdn.microsoft.com/en-us/library/aa922590.aspx.
+ * \ref adaptation http://msdn.microsoft.com/en-us/library/aa922590.aspx.
  */
-typedef struct
+typedef struct RGBTRIPLE
 {
     BYTE  rgbtBlue;
     BYTE  rgbtGreen;
     BYTE  rgbtRed;
-} __attribute__((__packed__))
-RGBTRIPLE;
+} __attribute__((__packed__)) RGBTRIPLE;
 
+
+#endif /* BMP_H */
